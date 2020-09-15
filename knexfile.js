d30cba4a3e0a4af75a01acf8effdf287c35839e2
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2020-09-14 21:04:25 -0300

    api rest com tdd

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+module.exports = {
+  test: {
+    client: 'pg',
+    version: '9.6',
+    connection: {
+      host: 'localhost',
+      user: 'adriane',
+      password: '123456',
+      database: 'barriga',
+    },
+    migrations: {
+      directory: 'src/migrations',
+    },
+  },
+};
---
+++ test/routes/user.test.js
@@ -0,0 +1,71 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+const mail = `${Date.now()}@mail.com`
+
+test('Deve listar todos os usuários', () => {
+  return request(app).get('/users')
+    .then((res) => {
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBeGreaterThan(0);
+    });
+});
+
+test('Deve inserir usuário com sucesso', () => {
+  return request(app).post('/users')
+    .send({ name: 'Walter Mitty', mail: mail, passwd: 'walter' })
+    .then((res) => {
+      expect(res.status).toBe(201);
+      expect(res.body.name).toBe('Walter Mitty');
+      expect(res.body).not.toHaveProperty('passwd')
+    });
+});
+
+test('Deve armazenar senha criptografada', async() => {
+  const res = await request(app).post('/users')
+    .send({ name: 'Walter Mitty', mail: `${Date.now()}@mail.com`, passwd: '123456' })
+  
+  expect(res.status).toBe(201)
+
+  const { id } = res.body;
+  const userDB = await app.services.user.findOne({id})
+  expect(userDB.passwd).not.toBeUndefined()
+  expect(userDB.passwd).not.toBe('123456')
+})
+
+test('Não deve inserir usuário sem nome', () => {
+  return request(app).post('/users')
+    .send({ mail: 'invalido@email.com', passwd: '123456'})
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Nome é um atributo obrigatório');
+    })
+});
+
+
+test('Não deve inserir usuário sem email', async () => {
+  const result = await request(app).post('/users')
+    .send({ name: "name", passwd: '123456'})
+  expect(result.status).toBe(400);
+  expect(result.body.error).toBe('Email é um atributo obrigatório');
+});
+
+test('Não deve inserir usuário sem senha', (done) => {
+  request(app).post('/users')
+    .send({ name: 'adriane', mail: 'adriane@artia.com' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Senha é um atributo obrigatório');
+      done();
+    }).catch(err => done.fail(err));
+})
+
+test('Não deve inserir usuário com email existente', () => {
+  return request(app).post('/users')
+    .send({ name: 'Walter Mitty', mail: mail, passwd: 'walter' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Já existe um usuário com esse email');
+    });
+});
